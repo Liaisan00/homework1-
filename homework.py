00d133478b3e9071e59605f8346e546f45843135
Liaisan Gibadullina <gibadullina95@icloud.com>
2020-10-05 18:00:00 +0300

    2 commit

--- homework.py
+++ homework.py
@@ -1,9 +1,11 @@
+
 class Birds():
     food = ("Птичий корм")
+    eggs = 'яйца'
     def feed(self):
-        self.feed = food
+        self.feed = self.food
     def collect(self):
-        self.collect = eggs
+        self.collect = self.eggs
 
 class Goose(Birds):
     voice = "Га-га-га"
@@ -27,15 +29,16 @@ class Duck(Birds):
 class Animals():
     food = ("Трава")
     def feed(self):
-        self.feed = food
+        self.feed = self.food
 
 class Cow(Animals):
     voice = "Му"
+    milk = 'молоко'
     def __init__(self, name, weight):
         self.name = name
         self.weight = weight
-    def milk(self):
-        self.milk = milk
+    def milking(self):
+        self.milking = self.milk
 
 class Sheep(Animals):
     voice = "Ме"
@@ -43,7 +46,7 @@ class Sheep(Animals):
         self.name = name
         self.weight = weight
     def cut(self):
-        self.cut = wool
+        self.cut = self.wool
 
 class Goat(Animals):
     voice = "Бе"
@@ -51,29 +54,32 @@ class Goat(Animals):
         self.name = name
         self.weight = weight
 
-Goose1 = Goose("Серый", 4)
-Goose2 = Goose("Белый", 3)
-Cow1 = Cow("Манька", 30)
-Sheep1 = Sheep("Барашек", 5)
-Sheep2 = Sheep("Кудрявый", 6)
-Chicken1 = Chicken("Ко-ко", 2)
-Chicken2 = Chicken("Кукареку", 1.5)
-Goat1 = Goat("Рога", 6)
-Goat2 = Goat("Копыта", 8)
-Duck1 = Duck("Кряква", 3)
-
-Animals = {"Серый": 4, "Белый": 3, "Манька": 30, "Барашек": 5, "Кудрявый": 6, "Ко-ко": 2, "Кукареку": 1.5,
-           "Рога": 6, "Копыта": 8, "Кряква": 3}
-
-#for values in Animals:
-    #print(f"Общий вес всех животных {sum(Animals.values())} кг")
 
 
-a = Animals.keys()
-b = Animals.values()
-Animals1 = zip(a,b)
-max_Animals = max(dict(Animals1))
-print(f"Самое тяжелое животное - {max_Animals}")
+
+# Goose1 = Goose("Серый", 4)
+# Goose2 = Goose("Белый", 3)
+# Cow1 = Cow("Манька", 30)
+# Sheep1 = Sheep("Барашек", 5)
+# Sheep2 = Sheep("Кудрявый", 6)
+# Chicken1 = Chicken("Ко-ко", 2)
+# Chicken2 = Chicken("Кукареку", 1.5)
+# Goat1 = Goat("Рога", 6)
+# Goat2 = Goat("Копыта", 8)
+# Duck1 = Duck("Кряква", 3)
+
+# Animals = {"Серый": 4, "Белый": 3, "Манька": 30, "Барашек": 5, "Кудрявый": 6, "Ко-ко": 2, "Кукареку": 1.5,
+#            "Рога": 6, "Копыта": 8, "Кряква": 3}
+
+# #for values in Animals:
+#     #print(f"Общий вес всех животных {sum(Animals.values())} кг")
+
+
+# a = Animals.keys()
+# b = Animals.values()
+# Animals1 = zip(a,b)
+# max_Animals = max(dict(Animals1))
+# print(f"Самое тяжелое животное - {max_Animals}")
 
 
 
